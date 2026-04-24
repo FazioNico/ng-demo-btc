75155c1384fbbbc3011da7c282f161cfcd4474f1
FazioNico <contact@nicolasfazio.ch>
2026-04-24 10:18:23 +0200

    feat: implement reactive form with validation and add address functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { BTC } from './components/btc/btc';
 import { DumpBtc } from './components/dump-btc/dump-btc';
 import { HasWordPipe } from './pipes/has-word/has-word-pipe';
+import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BTC, DumpBtc, HasWordPipe],
+  imports: [RouterOutlet, BTC, DumpBtc, HasWordPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,7 +16,40 @@ export class App {
   protected ticker = 'bitcoin';
   protected price = signal<number>(0);
 
+  protected demoForm = new FormGroup({
+    firstName: new FormControl('', Validators.compose([
+      Validators.minLength(2),
+      Validators.maxLength(20),
+      Validators.required,
+    ])),
+    lastName: new FormControl(''),
+    adresses: new FormArray([
+      new FormGroup({
+        street: new FormControl(),
+        number: new FormControl(),
+        npa: new FormControl(),
+        city: new FormControl(),
+        country: new FormControl(),
+      })
+    ]),
+  });
+
   updatePrice($event: number) {
     this.price.update(()=> $event);
+  }
+
+  submit() {
+    console.log(this.demoForm);
+  }
+
+  addAddress() {
+    const arrayCtrl = this.demoForm.get('adresses') as FormArray;
+    arrayCtrl.push(new FormGroup({
+      street: new FormControl(),
+      number: new FormControl(),
+      npa: new FormControl(),
+      city: new FormControl(),
+      country: new FormControl(),
+    }));
   }
 }
